8f31603ba1ee86248b4220cb8d7f9935714ebfbd
David Sulpy <david@sulpy.com>
2014-11-06 17:41:41 -0600

    fixed some bugs in the singletonstreamer on module imports in python 2

--- ISStreamer/SingletonStreamer.py
+++ ISStreamer/SingletonStreamer.py
@@ -1,10 +1,13 @@
-from ISStreamer.Streamer import Streamer as streamer
+try:
+	from ISStreamer.Streamer import Streamer as streamer
+except ImportError:
+	import Streamer as streamer
 
 class Streamer:
 	instance = None
 	def __init__(self, bucket, client_key="", debug_level=0):
 		if Streamer.instance == None:
-			Streamer.instance = streamer(bucket=bucket, client_key=client_key, debug_level=debug_level)
+			Streamer.instance = streamer.Streamer(bucket=bucket, client_key=client_key, debug_level=debug_level)
 
 	def log(self, signal, value):
 		Streamer.instance.log(signal, value)
